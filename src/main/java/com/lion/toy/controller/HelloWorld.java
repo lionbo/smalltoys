57efe85dd12e8758b4fc39e517e264d3a353261e
yongbo.liu <lyb1985031@163.com>
2023-05-05 08:47:54 +0800

    add responseBody

--- src/main/java/com/lion/toy/controller/HelloWorld.java
+++ src/main/java/com/lion/toy/controller/HelloWorld.java
@@ -2,6 +2,7 @@ package com.lion.toy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
 
     @RequestMapping("/world")
+    @ResponseBody
     public String helloWorld(){
         return "Hello World!";
     }
